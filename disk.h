46335c170bcff4399f19c3df946b433dd5353d8d
moonpotato <git@funnelweb.xyz>
2017-07-21 19:34:56 +0800

    Specify disk module interface

--- disk.h
+++ disk.h
@@ -20,8 +20,21 @@ typedef uint8_t disk_block; // Compatible with mem_block
 
 #define DISK_MAX_DISKS 256 // Chosen arbitrarily
 
+typedef enum _disk_action {
+    DA_NONE,
+    DA_ADDR,
+} disk_action;
+
+typedef enum _disk_state {
+    DS_OK,
+    DS_WAIT,
+    DS_ERROR,
+} disk_state;
+
 /////////////////////////////////////////////////////////////////////////
 // Function declarations
 /////////////////////////////////////////////////////////////////////////
 
+extern error_t disk_install(const char *filename, disk_id *num);
 
+extern error_t disk_remove(disk_id num);
