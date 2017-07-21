0d12996dd047b9cdd21681100395c5c733e1f599
moonpotato <git@funnelweb.xyz>
2017-07-22 00:01:59 +0800

    Load disks from the command line

--- main.c
+++ main.c
@@ -11,6 +11,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stddef.h>
+
+static size_t n_disks;
+static disk_id *loaded_disks;
+
+static void load_disks(int argc, char *argv[]);
+static void unload_disks();
 
 int main(int argc, char *argv[])
 {
@@ -19,17 +26,39 @@ int main(int argc, char *argv[])
 	DIE_ON(firmware_load(0x0, "fw.bin"));
 
 	// Install core I/O ports
-	// The only way these report failure is if there are no available ports
-	// So don't bother checking errors at this stage-
-	install_system_handler();
-	install_textio_handler();
+	DIE_ON(install_system_handler());
+	DIE_ON(install_textio_handler());
 
-	// Install the main disk
-	disk_id main_disk;
-	DIE_ON(disk_install("potato.bin", &main_disk));
+	// Each argument passed on the command line becomes a loaded disk.
+	load_disks(argc - 1, &argv[1]);
 
 	mem_dump();
+
+	// Clean up now, in reverse order
+	unload_disks();
+
+	remove_textio_handler();
+	remove_system_handler();
 
 	return EXIT_SUCCESS;
 }
 
+void load_disks(int argc, char *argv[])
+{
+	n_disks = argc;
+	loaded_disks = calloc(n_disks, sizeof (disk_id));
+
+	for (size_t i = 0; i < n_disks; ++i) {
+        DIE_ON(disk_install(argv[i], &loaded_disks[i]));
+	}
+}
+
+void unload_disks()
+{
+	for (size_t i = 0; i < n_disks; ++i) {
+        disk_remove(loaded_disks[i]);
+	}
+
+	free(loaded_disks);
+	loaded_disks = NULL;
+}
