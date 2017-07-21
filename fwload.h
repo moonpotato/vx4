8aedc31c8c338d4aa150e23647dd65658e2a7d02
moonpotato <git@funnelweb.xyz>
2017-07-21 15:03:40 +0800

    Document fwload.h

--- fwload.h
+++ fwload.h
@@ -14,5 +14,8 @@
  * IN file: The path to the firmware file to load.
  *
  * Returns:
+ * ERR_NOERR: The file was copied into memory successfully.
+ * ERR_FILE: The file doesn't exist or otherwise can't be accessed.
+ * ERR_NOMEM: A buffer to hold the file could not be allocated.
  */
 extern error_t firmware_load(maddr_t loc, const char *filename);
