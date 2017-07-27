63427341a9f8e39d86f152616806802596a14fd7
moonpotato <git@funnelweb.xyz>
2017-07-27 21:54:12 +0800

    Correctly include string.h for memcpy

--- graphics.c
+++ graphics.c
@@ -11,6 +11,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 ////////////////////////////////////////////////////////////////////////////////
 // Internal constants + helper macros
