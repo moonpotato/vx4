1d2dd4ff4d26c6841e3eb07cfeef09e2d675e9a2
moonpotato <git@funnelweb.xyz>
2017-07-27 16:07:20 +0800

    Fix argument names on stack_* functions to reflect purpose

--- stack.h
+++ stack.h
@@ -20,10 +20,10 @@ extern error_t stack_enter_frame();
 extern error_t stack_leave_frame();
 
 extern error_t stack_push(uint32_t word);
-extern error_t stack_push_multi(const uint32_t *words, mem_size bytes);
+extern error_t stack_push_multi(const uint32_t *words, mem_size num);
 
 extern error_t stack_pop(uint32_t *word);
-extern error_t stack_pop_multi(uint32_t *words, mem_size bytes);
+extern error_t stack_pop_multi(uint32_t *words, mem_size num);
 
-extern error_t stack_skip(mem_size bytes);
-extern error_t stack_unskip(mem_size bytes);
+extern error_t stack_skip(mem_size num);
+extern error_t stack_unskip(mem_size num);
